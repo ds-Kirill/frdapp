89fbd2b696d6c9da9c564506b159a9e5ee2f4a98
ds-Kirill <73669383+ds-Kirill@users.noreply.github.com>
2024-02-10 23:12:36 +0300

    Create test.py
    add test.py

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from ../main  import app
+
+client = TestClient(app)
+
+def test_info():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"tx_id": 1510422180, "is_fraud": 0}
+    print (response.json())
